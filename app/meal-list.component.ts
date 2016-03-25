5b5fbb738168434bec686514e2d49d42f363a7b0
Joshua Gustafson <silversevenfox@yahoo.com>
2016-03-25 09:25:27 -0700

    fix meal list component

---
+++ app/meal-list.component.ts
@@ -0,0 +1,52 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { DisplayMealComponent } from './meal-display.component';
+import { Meal } from './meal.model';
+import { NewMealComponent } from './new-meal.component';
+import { EditMealComponent } from './edit-meal.component';
+import { CaloriesPipe } from './calorie.pipe';
+
+@Component({
+  selector: 'meal-list',
+  inputs: ['mealList'],
+  outputs: ['onMealSelect'],
+  pipes: [CaloriesPipe],
+  directives: [DisplayMealComponent, NewMealComponent, EditMealComponent],
+  template:`
+  <select (change)="onChange($event.target.value)" class="filter">
+    <option *ngFor="#currentMeal of mealList" value="{{currentMeal.calories}}">{{currentMeal.calories}}</option>
+  </select>
+  <meal-display *ngFor="#currentMeal of mealList | calories:caloriesFilter"
+    (click)="mealClicked(currentMeal)"
+    [class.selected]="currentMeal === selectedMeal"
+    [meal]="currentMeal">
+  </meal-display>
+  <new-meal (onSubmitNewMeal)="createMeal($event)">
+  </new-meal><br>
+  <edit-meal
+    *ngIf="selectedMeal"
+    [meal]="selectedMeal"
+    >
+  </edit-meal>
+  `
+})
+export class MealListComponent {
+  public mealList: Meal[];
+  public onMealSelect: EventEmitter<Meal>
+  public selectedMeal: Meal;
+  public caloriesFilter: string = 'none';
+  constructor() {
+    this.onMealSelect = new EventEmitter();
+  }
+  mealClicked(clickedMeal: Meal): void {
+    this.selectedMeal = clickedMeal;
+    this.onMealSelect.emit(clickedMeal);
+  }
+  createMeal(newMeal: Meal): void {
+    this.mealList.push(
+      new Meal(newMeal.name, newMeal.details, newMeal.calories, this.mealList.length)
+    );
+  }
+  onChange(filterOption) {
+    this.caloriesFilter = filterOption;
+  }
+}
