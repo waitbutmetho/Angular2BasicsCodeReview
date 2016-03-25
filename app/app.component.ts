1ba527793193f5fb54d9fcd45cba63c075198786
Joshua Gustafson <silversevenfox@yahoo.com>
2016-03-25 09:05:52 -0700

    create my-app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,34 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
+import { MealListComponent         } from './meal-list.component';
+import { Meal                      } from './meal.model';
 
 // defining annotation aka "decorator"
 @Component({
   selector: 'my-app',
+  directives: [ MealListComponent ],
   template: `
   <div class="container">
-    <h1>Skeleton Angular2 App!</h1>
+  <h1>Meal and Calorie Tracker</h1>
+    <div class="meal">
+      <meal-list [mealList]="meals" (onMealSelect)="mealWasSelected($event)">
+      </meal-list>
+    </div>
   </div>
-  ` //uses backticks
+  `
 })
 
 export class AppComponent {
+  public meals: Meal[];
 
+  constructor(){
+    this.meals = [
+      new Meal("Big Mac", 'Ate the whole thing', 563, 0),
+      new Meal("Large French Fries", 'Ate all of them', 498, 1),
+      new Meal("Oreo McFlurry", 'It was delicious', 560, 2),
+      new Meal("McDouble", 'Only ate half', 195, 3)
+    ];
+  }
+  mealWasSelected(clickedMeal: Meal): void {
+    console.log('Parent', clickedMeal);
+  }
 }
